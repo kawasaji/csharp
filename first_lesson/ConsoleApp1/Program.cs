a6ad23aaf381516ed57dc8b35bbfee997c09c382
Гусейнли Джамиль Вюгар <Gusey_sk41@itstep.edu.az>
2022-12-21 12:03:28 +0400

    first lesson

---
+++ first_lesson/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿// to input data from console we use Console.ReadLine();
+//string name = Console.ReadLine();
+//Console.WriteLine(name);
+
+//int age = Int32.Parse(Console.ReadLine());
+//Console.WriteLine(age);
+
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(age);
+
+//int age = 0;
+//Int32.TryParse(Console.ReadLine(), out age);
+//Console.WriteLine(age + 1);
+
+
+
+//Console.Write("Enter name ~# ");
+//var name = Console.ReadLine(); 
+
+//Console.Write("Enter surname ~# ");
+//var surname = Console.ReadLine(); 
+
+//Console.Write("Enter age ~# ");
+//var age = Console.ReadLine();
+
+//// String Interpolation.
+//Console.WriteLine(
+//    $"Name is: {name}\n" +
+//    $"Surname us: {surname}\n" +
+//    $"Age is: {age}\n");
+
+
+
+// Class
+//Maga a = new Maga();
+
+//var a = new Maga();
+
+//Maga a = new();
+//class Maga
+//{
+
+//}
+
+
+// Array
+int[] arr  = new int[10];
+
+Console.WriteLine(arr.GetType()); // Получить тип данных (присутсвуется во всех типах данных)
+
+arr[0] = 1;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+
+// Jagged array
+int[][] arr_a = new int[2][]
+{
+    new int []{1, 2, 3, 4, 5, 6},
+    new int[]{7, 8 ,9}
+};
+
+int[,]
